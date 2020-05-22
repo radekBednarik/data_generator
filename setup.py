621a6de647fa7fea44cfd9d4bfbaf5fb1cc9333e
bednaJedna <46536898+bednaJedna@users.noreply.github.com>
2020-05-22 11:12:32 +0200

    Update setup.py
    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="Data Generator",
-    version="0.1.0",
+    version="0.1.0.1",
     packages=find_packages(),
     install_requires=["tqdm"],
     author="bednaJedna",
