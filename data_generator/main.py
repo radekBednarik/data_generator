a82b4f918a80425068ba1d95fdbad673d1fef42c
radekBednarik <radek.bednarik@tesena.com>
2020-05-25 20:24:30 +0200

    refactored

--- data_generator/main.py
+++ data_generator/main.py
@@ -30,14 +30,14 @@ def run_cli_inputs(args: argparse.Namespace) -> Union[tuple, int]:
     return 1
 
 
-def run_toml_inputs(args: argparse.Namespace) -> Union[List[tuple], List[int]]:
+def run_toml_inputs(args: argparse.Namespace) -> Union[List[int], List[tuple]]:
     """Retruns data generators and other needed args, when user uses TOML files to provide inputs.
 
     Arguments:
         args {argparse.Namespace} -- args entered via CLI
 
     Returns:
-        Union[List[tuple], int] -- list of (data generators dict, cli args dict), 1: if NOK
+        Union[List[tuple], List[int]] -- list of (data generators dict, cli args dict), 1: if NOK
     """
     output = []
 
@@ -49,14 +49,11 @@ def run_toml_inputs(args: argparse.Namespace) -> Union[List[tuple], List[int]]:
         if len(converted_args["toml"]) > 0:
             for filepath in converted_args["toml"]:
                 conf_dict = get_input(filepath)
-                # hack
-                converted_args["rows"] = conf_dict["rows"]
-
                 result = assemble_data_generators(conf_dict)
 
                 print("--> Data generators created.\r\n")
 
-                output.append((result, converted_args))
+                output.append((result, conf_dict))
             return output
         return [1]
     except Exception as e:
@@ -64,18 +61,17 @@ def run_toml_inputs(args: argparse.Namespace) -> Union[List[tuple], List[int]]:
         return [1]
 
 
-def run_outputs(args: argparse.Namespace, inputs: tuple) -> None:
+def run_outputs(inputs: Union[tuple, int]) -> None:
     """Generates data via generators and saves them to specified file format.
 
     Arguments:
-        args {argparse.Namespace} -- args entered via CLI
         inputs {tuple} -- (data generators dict, cli args dict)
     """
     print("--> Data generation and saving starting... \n")
 
-    if args.save_as == "json":
+    if inputs[1]["save_as"] == "json":
         to_json(inputs[0], inputs[1]["rows"], inputs[1]["folder"])
-    elif args.save_as == "xlsx":
+    elif inputs[1]["save_as"] == "xlsx":
         to_excel(inputs[0], inputs[1]["rows"], inputs[1]["folder"])
     else:
         to_csv(inputs[0], inputs[1]["rows"], inputs[1]["folder"])
@@ -90,12 +86,12 @@ def main() -> None:
 
     if hasattr(args, "specify"):
         output = run_cli_inputs(args)
-        run_outputs(args, output)
+        run_outputs(output)
 
     if hasattr(args, "toml"):
         outputs = run_toml_inputs(args)
         for output in outputs:
-            run_outputs(args, output)
+            run_outputs(output)
 
 
 if __name__ == "__main__":
