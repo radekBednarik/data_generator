9fd67b8595425d598db556ee904a5dab4cf30280
bednaJedna <bednarik.radek@gmail.com>
2020-05-27 15:40:38 +0200

    bump up version - fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="Data Generator",
-    version="0.5.0",
+    version="0.5.1",
     packages=find_packages(),
     install_requires=["tqdm", "XlsxWriter", "tomlkit"],
     author="bednaJedna",
